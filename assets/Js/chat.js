d55cc9e9a0da74e321a03363b57a1a9b42dd80a5
AlessandroPuccini <puccini.alessandro@gmail.com>
2024-04-22 00:34:36 +0200

    aggiunta messaggi automatici utente

--- assets/Js/chat.js
+++ assets/Js/chat.js
@@ -1,3 +1,5 @@
+import { crea } from "./myDom.js";
+
 const chatBody = document.querySelector("#chat-body"),
     chatTextarea = document.querySelector("#chat-text"),
     btn = {
@@ -10,6 +12,7 @@ const chatBody = document.querySelector("#chat-body"),
 
     ],
     newMessageEvt = new EventTarget();
+    const newMessageSound = new Audio("./assets/sound/new-message.mp4");
 
 
 export default function chat() {
@@ -19,18 +22,52 @@ export default function chat() {
 function setEvent(){
   btn.send.addEventListener("click", sendMessage);
   chatTextarea.addEventListener("keyup", textareaevent);
+  newMessageEvt.addEventListener('newUserMessage', newMessageUser);
 }
+
+function newMessageUser(){
+    addMessageInPage(messageList[messageList.length - 1]);
+}
+
 function textareaevent(e){
     if(e.key === "Enter"){
-        console.log("Ho digitato qualcosa")
+        sendMessage(e);
     }
   
 }
 
 function sendMessage(e){
-  console.log("Devo inviare il messaggio");
+  const messageText = chatTextarea.value;
+    const messageObj = { by: 'admin', message: messageText };
+
+    if(!messageText) return; 
+
+    messageList.push(messageObj);
+    addMessageInPage(messageObj);
+    chatTextarea.value = "";
+    simulateUserMessage();
+}
+
+function simulateUserMessage(){
+    setTimeout(() => {
+        messageList.push({ by: 'user', message: 'Ciao!'});
+        newMessageEvt.dispatchEvent(new Event('newUserMessage'));
+    }, 2000);
 }
 
 function showMessage(){
-    console.log("mostro i messaggi");
+    messageList.forEach(msg => {
+      addMessageInPage(msg);
+    });
+}
+function addMessageInPage(msg){
+  const div = crea("div", [["class", `message ${msg.by}`]]),
+        span = crea("span", [['class', 'time']]),
+        p = crea("p", [], msg.message);
+
+    div.append(span, p);
+    chatBody.append(div);
+    chatBody.scrollTo(0, chatBody.scrollHeight - chatBody.clientHeight); 
+
+    if(msg.by === 'user') newMessageSound.play();
 }
